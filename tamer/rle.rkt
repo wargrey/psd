6a963e038b662c47c803408b92378d3a8e38c92f
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2017-07-11 16:54:23 +0800

    rewrite rle.rkt with Typed Racket

---
+++ tamer/rle.rkt
@@ -0,0 +1,17 @@
+#lang typed/racket/base
+
+(require "../digitama/rle.rkt")
+
+(require typed/rackunit)
+(require typed/rackunit/text-ui)
+
+(define in : Bytes (bytes #xFE #xAA #x02 #x80 #x00 #x2A #xFD #xAA #x03 #x80 #x00 #x2A #x22 #xF7 #xAA))
+(define out : Bytes (bytes #xAA #xAA #xAA #x80 #x00 #x2A #xAA #xAA #xAA #xAA #x80 #x00
+                           #x2A #x22 #xAA #xAA #xAA #xAA #xAA #xAA #xAA #xAA #xAA #xAA))
+
+(define tests-from-wikipedia
+  (test-suite
+   "Run-length Encoding"
+   (check-equal? out (unpackbits in))))
+
+(run-tests tests-from-wikipedia)
