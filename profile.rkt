55212a32d214d229574c12650a7463afae611fd0
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2017-07-26 23:23:03 +0800

    add #:full? to (psd-profile)

--- profile.rkt
+++ profile.rkt
@@ -11,8 +11,8 @@
 (require "digitama/resource.rkt")
 (require "digitama/layer.rkt")
 
-(define psd-profile : (->* (PSD) (Output-Port #:prefix String) Void)
-  (lambda [self [out (current-output-port)] #:prefix [prefix ""]]
+(define psd-profile : (->* (PSD) (Output-Port #:prefix String #:full? Boolean) Void)
+  (lambda [self [out (current-output-port)] #:prefix [prefix ""] #:full? [full? #true]]
     (define-values (~t ~t~t) (values (string-append prefix "    ") (string-append prefix "        ")))
 
     (fprintf out "~a~a Object[~a]:~n" prefix (if (PSD? self) 'PSD 'PSB) (PSD-File-name self))
@@ -22,18 +22,20 @@
     (fprintf out "~aCompression Method: ~a~n" ~t (PSD-Header-compression-method self))
 
     (define resources : PSD-Image-Resources (psd-image-resources self #:resolve? #false))
+    (define layers : (Listof PSD-Layer-Object) (psd-layers self))
     (define mask : (Option PSD-Global-Layer-Mask) (psd-global-layer-mask self))
     (define infobase : PSD-Layer-Infobase (psd-tagged-blocks self #:resolve? #false))
+    
     (fprintf out "~aResources: ~a~n" ~t (hash-keys resources))
+    (fprintf out "~aLayer Count: ~a~n" ~t (length layers))
     (fprintf out "~aGlobal Mask: ~a~n" ~t (if (not mask) 'None (vector-drop (struct->vector mask) 2)))
     (fprintf out "~aTagged Blocks: ~a~n" ~t (hash-keys infobase))
-
-    (newline out)
-    (define layers : (Listof PSD-Layer-Object) (psd-layers self))
-    (fprintf out "~aLayer Count: ~a~n" ~t (length layers))
-    (for ([layer (in-list layers)])
-      (psd-layer-profile layer out #:prefix ~t)
-      (newline out))))
+    
+    (unless (not full?)
+      (newline out)
+      (for ([layer (in-list layers)])
+        (psd-layer-profile layer out #:prefix ~t)
+        (newline out)))))
 
 (define psd-layer-profile : (->* (PSD-Layer-Object) (Output-Port #:prefix String) Void)
   (lambda [self [out (current-output-port)] #:prefix [prefix ""]]
